ba3a9a0664ae82be212fecab78f55d203ca8c569
holawan <asdf134652@gmail.com>
2022-06-08 21:53:03 +0900

    주사위쌓기

--- BAEKJOON/Resolving/2116_주사위 쌓기.py
+++ BAEKJOON/Resolving/2116_주사위 쌓기.py
@@ -1,14 +1,25 @@
-import sys
+N = int(input())
+lst = []
+for _ in range(N):
+    lst.append(list(map(int, input().split())))
+dice = {0 : 5, 1 : 3, 2 : 4, 3 : 1, 4 : 2, 5 : 0} 
+# print(lst)
+res = 0 
+for i in range(6):
+    result = [] 
+    tmp = [1, 2, 3, 4, 5, 6] 
+    tmp.remove(lst[0][i]) 
+    tmp2 = lst[0][dice[i]]
+    tmp.remove(tmp2) 
+    result.append(max(tmp)) 
+    for j in range(1, N): 
+        tmp = [1, 2, 3, 4, 5, 6]
+        tmp.remove(tmp2)
+        tmp2 = lst[j][dice[lst[j].index(tmp2)]]
+        tmp.remove(tmp2)
+        result.append(max(tmp)) 
+    result = sum(result) 
+    if res < result: 
+        res = result
 
-a,b = map(int,sys.stdin.readline().split())
-
-lst = list(map(int,sys.stdin.readline().split()))
-
-
-M = []
-M.append(sum(lst[0:b]))
-
-for i in range((a-b)):
-    S = M[i]-lst[i]+lst[i+b]
-    M.append(S)
-print(max(M))
+print(res)
